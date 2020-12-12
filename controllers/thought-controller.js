c4689449c7a92e784b0684516a5e3c100282e9af
Alexandra <anance5@murraystate.edu>
2020-12-11 19:29:29 -0600

    Create User and Thought controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,21 @@
+const { Thought } = require('../models');
+
+const thoughtController = {
+    // get all thoughts
+
+
+    // get one thought by id
+
+
+    // create thought
+
+
+    // update thought by id
+
+
+    // remove thought by id
+
+    
+};
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,21 @@
+const { User } = require('../models');
+
+const userController = {
+    // get all users
+
+
+    // get one user by id
+
+
+    // create user
+
+
+    // update user by id
+
+
+    // remove user by id
+
+    
+};
+
+module.exports = userController;
