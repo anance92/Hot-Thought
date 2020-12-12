f09196e50222ef9257405d20cdb1952bce6868e0
Alexandra <anance5@murraystate.edu>
2020-12-11 19:50:59 -0600

    Connect routes to controllers

---
+++ routes/api/user-routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controller');
+const { route } = require('./thought-routes');
+
+// Set up GET all and POST at /api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// Set up GET one, PUT, and DELETE at /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// Set up POST and DELETE at api/users/:userId/friends/:friendId
+router
+    .route('/:userId/friends/:friendId')
+    .post()
+    .delete();
+
+module.exports = router;
